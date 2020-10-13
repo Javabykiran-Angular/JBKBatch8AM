d5cfb7a6e75ae46d14a68e655ffb22ac1fc98d40
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2020-10-13 09:35:31 +0530

    Day7

---
+++ Typescript/Day7/demo1.js
@@ -0,0 +1,42 @@
+//Array 
+var a = [10, 20, 30, 40, 50];
+var a1 = ['Angular 8', 'React JS', 'Vue JS', 'Embber Js'];
+var a2; //Update in version 4.0.2 by default its undefined 
+var a3 = [];
+// for(let i=0;i<a.length;i++){
+//    // console.log("Array Value is "+a[i]);
+//    console.log(`
+//     ----------------------------------------
+//       Array Value is ${a[i]}  
+//     ----------------------------------------  
+//    `); //back tick operator
+// }
+// console.log(`
+// ==========================
+// Array value is ${a1}
+// `);
+//console.log("Array display using Join Method "+a.join(' '));
+// a.forEach((myvalue,i)=>{
+//     console.log(`
+//     value is ${myvalue} index ${i}
+//     `);
+// });
+//LIFO Principal push()
+//Splice() 
+console.log(a3);
+a3.push(12);
+console.log(a3);
+a3.push(13);
+console.log(a3);
+a3.push(14);
+console.log(a3);
+var temp1 = a3.pop();
+console.log(a3);
+console.log("Temp value is " + temp1);
+console.log("value of a2 " + a2);
+//a2.push(21);
+//console.log("value of a2 is "+a2);
+var temp2 = ['Sumit', 12, true, 33, 'raokhande'];
+temp2[0] = 'ffffff';
+temp2[0] = 23;
+console.log(temp2);
