54da6e05b8ad3ab8fa046553db58d62a43193896
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2020-10-14 09:20:10 +0530

    Day8

---
+++ Typescript/Day8/demo1.js
@@ -0,0 +1,35 @@
+var a = [];
+//using splice function 
+// a.splice(0,0,10);
+// console.log(a); //10
+// a.splice(0,0,20,30,40);
+// console.log(a);
+// a.splice(1,0,70)
+// console.log(a);
+// a.splice(2,1);
+// console.log(a);
+// a.splice(2,2);
+// console.log(a);
+// a.splice(1,0,45,55,56,78);
+// console.log(a);
+// a.splice(3,1,12);
+// console.log(a);
+// a.splice(3,2,13,15);
+// console.log(a);
+//slice
+var a1 = [20, 33, 4, 55, 66];
+var temp = a1.slice(1, 3);
+// console.log("Original Array "+a1);
+// console.log("Copied Data "+temp);
+// var temp2=a1.slice();
+// console.log("==============================");
+// console.log("Original Array "+a1);
+// console.log("Copied Data "+temp2);
+// var temp3=a1.slice(1);
+// console.log("==============================");
+// console.log("Original Array "+a1);
+// console.log("Copied Data "+temp3);
+var temp4 = a1.slice(-1, -2);
+console.log("===== " + temp4);
+var temp5 = a1.slice().reverse();
+console.log("===== " + temp5);
